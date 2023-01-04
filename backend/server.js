cd11d77f36d86ca1cbf178c3298b5707301ee0bb
Brayden Kim <dudgjs1318@gmail.com>
2023-01-04 11:37:46 -0800

    front&back

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+
+// express
+const app = express();
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONG_URL)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,92 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { useState } from "react";
+
+const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext();
+    const [isEdit, setIsEdit] = useState(false);
+
+    const [title, setTitle] = useState(workout.title);
+    const [load, setLoad] = useState(workout.load);
+    const [reps, setReps] = useState(workout.reps);
+
+    // each elements came from line 11 - 13
+    const workoutEdit = { title, load, reps }
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE',
+        })
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
+        }
+        console.log('json delete', json);
+
+    }
+
+    const handleEditClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'PATCH',
+            body: JSON.stringify(workoutEdit),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({ type: 'EDIT_WORKOUT', payload: json })
+        }
+        console.log('json edit', json);
+    }
+
+    const handleEditClickIcon = () => {
+        setIsEdit(!isEdit);
+    }
+
+    return (
+        <div className="workout-details">
+            <div className="layout">
+                <h4 className="title">{workout.title}</h4>
+                <h4 className="material-symbols-outlined" onClick={handleEditClickIcon}>edit</h4>
+            </div>
+            {isEdit &&
+                <form className="edit" onSubmit={handleEditClick}>
+                    <label>Exercise Title:</label>
+                    <input
+                        type="text"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                    <label>Load (in kg):</label>
+                    <input
+                        type="number"
+                        onChange={(e) => setLoad(e.target.value)}
+                        value={load}
+                    />
+                    <label>Reps:</label>
+                    <input
+                        type="number"
+                        onChange={(e) => setReps(e.target.value)}
+                        value={reps}
+                    />
+                    <button>Edit Workout</button>
+                </form>
+            }
+            {!isEdit &&
+                <>
+                    <p><strong>Load (kg): </strong>{workout.load}</p>
+                    <p><strong>Reps: </strong>{workout.reps}</p>
+                    <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+                    <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+                </>
+            }
+        </div>
+    )
+}
+
+export default WorkoutDetails;
